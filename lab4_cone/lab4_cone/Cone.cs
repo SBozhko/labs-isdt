80e0c132c11338f91a4a48f96c2ab2934815dd72
SBozhko <svb@name.com>
2013-02-12 01:44:41 +0300

    add entity class Cone

---
+++ lab4_cone/lab4_cone/Cone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab4_cone
+{
+    class Cone
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+        public double Density { get; set; }
+        public double Volume { get { return Math.PI * Math.Pow(Radius, 2) * Height / 3;} }
+        public double Mass { get { return Volume * Density;} }
+
+        public Cone()
+        { 
+        }
+    }
+}
