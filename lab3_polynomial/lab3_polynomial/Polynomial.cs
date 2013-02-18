4dbb73707cfa0639bca4a15977b92f6b0de99cf6
SBozhko <svb369@gmail.com>
2013-02-18 13:51:44 +0300

    add to class Polynomial ToStrings methods

--- lab3_polynomial/lab3_polynomial/Polynomial.cs
+++ lab3_polynomial/lab3_polynomial/Polynomial.cs
@@ -24,15 +24,61 @@ namespace lab3_polynomial
         public double Calculate(double x)
         {
             double result = 0;
-            double tmpX = 1;
+            double xDegree = 1;
             for (int i = Coefficients.Length - 1; i >= 0; i--)
             {                
-                result += Coefficients[i] * tmpX;
-                tmpX *= x;                                                                           
+                result += Coefficients[i] * xDegree;
+                xDegree *= x;                                                                           
             }
 
             return result;
         }
+
+        public double GetCoefficient(int index)
+        {
+            if (index <= 0 || index > Coefficients.Length)
+            { 
+                // TODO exception
+            }
+            return Coefficients[index - 1];
+        }
+
+        public string ToHumanString()
+        {
+            StringBuilder sb = new StringBuilder(base.ToString() + "\n");
+            int xDegree = this.degree;
+            for (int i = 0; i < Coefficients.Length; i++)
+            {
+
+                string coeffStr = (Coefficients[i] != 1 && Coefficients[i] != 0) ? Coefficients[i].ToString() : "";
+               
+                string xDegreeStr = (xDegree != 0 && xDegree != 1) ? "x^" + xDegree.ToString() : "";
+                xDegreeStr = (xDegree == 1) ? "x" : xDegreeStr;
+                xDegreeStr = (Coefficients[i] == 0) ? "" : xDegreeStr;
+
+                sb.Append(coeffStr + xDegreeStr);
+                sb.Append((i < Coefficients.Length - 1 && Coefficients[i] != 0) ? " + " : "");
+                xDegree--;
+            }
+            
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder(base.ToString() + "\n");
+            int xDegree = this.degree;
+            for (int i = 0; i < Coefficients.Length; i++)
+            {
+                sb.Append(Coefficients[i] + "x^" + xDegree);
+                sb.Append((i < Coefficients.Length - 1) ? " + " : "");
+                xDegree--;
+            }
+
+            return sb.ToString();
+        }
+
+
 
     }
 }
