e8a11544bf584bab8cc77113d3370a03b461ea3e
SBozhko <svb369@gmail.com>
2013-02-19 10:51:02 +0300

    create project for lab2_array

---
+++ lab2_array/lab2_array/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab2_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
